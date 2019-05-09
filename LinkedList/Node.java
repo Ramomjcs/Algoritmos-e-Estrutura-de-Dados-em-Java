ef74e2168933e06518e57dcc6d39a2ec04307969
Usori <alphaxtech@hotmail.com>
2019-05-09 05:51:36 -0300

    Classe Node
    Classe utilizada na manipulação da lista ligada

---
+++ LinkedList/Node.java
@@ -0,0 +1,8 @@
+class Node{
+  int data; // pode ser qualquer tipo de armazenamento de dados, String, double, outro objeto, etc...
+  Node next;
+  
+  Node(int data){
+    this.data = data;
+  }
+}
